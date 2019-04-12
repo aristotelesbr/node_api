11369151262cb4424efa9fb9fa0f9993a40da76c
Aristoteles <aristotelesbr2014@gmail.com>
2019-04-11 22:29:07 -0300

    Added mongoose

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/test',(req, res) => {
-  return res.send('Hello Worlc)
+  return res.send('Hello World a partir do routes.');
 })
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// import mongoose
+mongoose.connect('mongodb+srv://ari:ari@cluster0-2kafy.mongodb.net/dropbox_development?retryWrites=true', 
+  {
+    useNewUrlParser: true
+  }
+);
 // Retirna no formato JSON
 app.use(express.json());
 // Permite o upload de arquivos
