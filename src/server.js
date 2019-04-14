59e3615b1c1651d3a26ecbe31fa8822cf71690a6
Aristoteles <aristotelesbr2014@gmail.com>
2019-04-14 11:29:00 -0300

    Added environment variables

--- src/server.js
+++ src/server.js
@@ -18,10 +18,9 @@ io.on('connection', (socket) => {
 });
 
 // Conexão com o banco
-mongoose.connect('mongodb+srv://ari:ari@cluster0-2kafy.mongodb.net/dropbox_development?retryWrites=true',
-  {
-    useNewUrlParser: true,
-  });
+mongoose.connect('mongodb+srv://ari:ari@cluster0-2kafy.mongodb.net/dropbox_development?retryWrites=true', {
+  useNewUrlParser: true,
+});
 
 app.use((req, res, next) => {
   req.io = io;
@@ -31,9 +30,11 @@ app.use((req, res, next) => {
 // Retirna no formato JSON
 app.use(express.json());
 // Permite o upload de arquivos
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true,
+}));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 // Importando o arquivo de routes
 app.use(require('./routes'));
 
-server.listen(3333, console.log('Rodando na porta 3333'));
+server.listen(process.env.PORT || 3333, console.log('Servidor Rodando na porta 3333'));
