e04c120ebf0b64127f87b5f7e22f4232134124d1
Aristoteles <aristotelesbr2014@gmail.com>
2019-04-14 11:12:44 -0300

    added cors settigns

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+// Cors
+app.use(cors());
 
 const server = require('http').Server(app);
-
 const io = require('socket.io')(server);
 
 // Cria socket
